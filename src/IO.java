724073a54303b3dad6d0baa3552ee41640256b70
stefanberg96 <bergshm@gmail.com>
2018-03-01 19:11:07 +0100

    input read from file, file is as argument

--- src/IO.java
+++ src/IO.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,10 +13,11 @@ public class IO {
     int nbRides = 0;
     int bonus = 0;
     int steps = 0;
-    List<Coordinate>
+    List<Ride> rides = new ArrayList<>();
 
-    public  void readInput(){
-        Scanner scanner =new Scanner(System.in);
+    public  void readInput(File file) throws FileNotFoundException {
+
+        Scanner scanner =new Scanner(new FileReader(file));
         this.rows = scanner.nextInt();
         this.columns = scanner.nextInt();
         this.vehicles = scanner.nextInt();
@@ -21,8 +26,11 @@ public class IO {
         this.steps = scanner.nextInt();
 
         for(int i =0 ; i<nbRides;i++){
-            
+            Coordinate start = new Coordinate(scanner.nextInt(), scanner.nextInt());
+            Coordinate finish  = new Coordinate(scanner.nextInt(),scanner.nextInt());
+            rides.add(new Ride(start,finish,scanner.nextInt(),scanner.nextInt()));
         }
+        System.out.println("Read input");
     }
 
     public static void outPut(Object toOutput){
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws FileNotFoundException {
+        if(args.length<1){
+            System.out.println("You didnt specify a file");
+            return;
+        }
+
+        IO io = new IO();
+        File file = new File(args[0]);
+        io.readInput(file);
     }
 }
